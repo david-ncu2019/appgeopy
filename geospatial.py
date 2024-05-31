0a3fdd00a4d0766759ad1f4758edc5583769bd00
davidncu <vinhtruong1995@gmail.com>
2024-05-31 23:17:43 +0800

    Add `find_points_within_buffer` function

--- geospatial.py
+++ geospatial.py
@@ -1,6 +1,11 @@
+import warnings  # Warning control
+
 import geopandas as gpd
 from pyproj import CRS
 from shapely.geometry import Point
+
+# Filter Warnings
+warnings.filterwarnings("ignore")  # Suppress all warnings
 
 
 def convert_to_geodata(
@@ -50,3 +55,53 @@ def convert_to_geodata(
 
     except Exception as e:
         raise Exception(f"An error occurred during the conversion: {e}")
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+def find_points_within_buffer(
+    central_point, target_points_gdf, central_key_column, buffer_radius
+):
+    """
+    Find points within a buffer radius from a single central point and assign the central point's key to the target points.
+
+    Parameters:
+    central_point : GeoSeries
+        GeoSeries containing the central point.
+    target_points_gdf : GeoDataFrame
+        GeoDataFrame containing the target points.
+    central_key_column : str
+        The column name in central_point containing the key values to assign to target points.
+    buffer_radius : float
+        The radius of the buffer around the central point in the units of the GeoDataFrame's coordinate reference system.
+
+    Returns:
+    GeoDataFrame
+        A GeoDataFrame of target points within the buffer, with the central point's key assigned.
+
+    Example:
+
+    # Apply the point_within_buffer function to each row of central_gdf
+    results = central_gdf.apply(
+        lambda row: point_within_buffer(row, target_points_gdf, central_key_column, buffer_radius), axis=1
+    )
+
+    # Concatenate the results into a single GeoDataFrame
+    result_gdf = pd.concat(results.tolist(), ignore_index=True)
+
+    """
+    # Extract the key value from the central point
+    key_value = central_point[central_key_column]
+
+    # Create a buffer around the central point
+    central_point_buffer = central_point.geometry.buffer(buffer_radius)
+
+    # Find target points within the buffer
+    filter_cond = target_points_gdf.geometry.within(central_point_buffer)
+    selected_points = target_points_gdf[filter_cond].copy()
+
+    # Assign the central point's key value to the target points
+    selected_points[central_key_column] = key_value
+
+    return selected_points
